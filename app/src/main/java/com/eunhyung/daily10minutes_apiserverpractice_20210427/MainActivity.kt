a2d3813eadcff6fb99e7524f4b53435933cef96f
전은형 <ooooo5h91@gmail.com>
2021-04-27 23:19:04 +0900

    입력한 이메일/비밀번호 추출

--- app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/MainActivity.kt
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/MainActivity.kt
@@ -2,6 +2,7 @@ package com.eunhyung.daily10minutes_apiserverpractice_20210427
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,14 @@ class MainActivity : BaseActivity() {
 
     }
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPassword = passwordEdt.text.toString()
+
+
+        }
 
     }
 
