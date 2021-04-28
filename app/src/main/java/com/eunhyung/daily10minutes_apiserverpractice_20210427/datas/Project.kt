dc71b2fe15771789e278d85e12b6abd673f8ef45
전은형 <ooooo5h91@gmail.com>
2021-04-28 22:53:52 +0900

    서버데이터 참고해서 프로젝트 정보 표기용 클래스 추가

---
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/datas/Project.kt
@@ -0,0 +1,8 @@
+package com.eunhyung.daily10minutes_apiserverpractice_20210427.datas
+
+class Project (
+    var id : Int,
+    var title : String,
+    var imgUrl : String,
+    var description : String) {
+}
