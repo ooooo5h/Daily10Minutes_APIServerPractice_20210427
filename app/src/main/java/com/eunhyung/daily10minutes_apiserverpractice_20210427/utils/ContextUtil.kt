260ba7e8b72180bea9646e56e197417e05984d33
전은형 <ooooo5h91@gmail.com>
2021-04-28 22:02:17 +0900

    자동로그인 여부 저장/ 조회하는 기능 추가

--- app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/utils/ContextUtil.kt
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/utils/ContextUtil.kt
@@ -1,9 +1,29 @@
 package com.eunhyung.daily10minutes_apiserverpractice_20210427.utils
 
+import android.content.Context
+
 class ContextUtil {
 
     companion object {
 
+        private val prefName = "Daily10MinutesPref"
 
+        private val AUTO_LOGIN = "AUTO_LOGIN"
+
+        fun setAutoLogin(context : Context, autoLogin : Boolean) {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+        }
+
+        fun getAutoLogin( context: Context) : Boolean {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            return pref.getBoolean(AUTO_LOGIN, false)
+
+        }
     }
+
 }
