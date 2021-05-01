e4e338ce40b4d06c4619e78e9ec9f2d1fe12fde3
전은형 <ooooo5h91@gmail.com>
2021-05-01 11:52:22 +0900

    리스트뷰 아이템 클릭하면 프로젝트 상세화면으로 진입

--- app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/utils/ViewProjectDetailActivity.kt
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/utils/ViewProjectDetailActivity.kt
@@ -2,10 +2,17 @@ package com.eunhyung.daily10minutes_apiserverpractice_20210427.utils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.eunhyung.daily10minutes_apiserverpractice_20210427.BaseActivity
 import com.eunhyung.daily10minutes_apiserverpractice_20210427.R
+import com.eunhyung.daily10minutes_apiserverpractice_20210427.datas.Project
+import kotlinx.android.synthetic.main.activity_view_project_detail.*
+import kotlinx.android.synthetic.main.project_list_item.*
 
 class ViewProjectDetailActivity : BaseActivity() {
+
+    lateinit var mProject : Project
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_project_detail)
@@ -17,6 +24,14 @@ class ViewProjectDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mProject = intent.getSerializableExtra("projectInfo") as Project
+
+        Glide.with(mContext).load(mProject.imgUrl).into(projectImg)
+        titleTxt.text = mProject.title
+        descriptionTxt.text = mProject.description
+
+
 
     }
 
