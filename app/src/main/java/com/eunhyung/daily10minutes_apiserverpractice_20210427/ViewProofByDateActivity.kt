4f352be4c68f2c9ec3680b47d3e3824b5fae0534
전은형 <ooooo5h91@gmail.com>
2021-05-02 18:41:40 +0900

    인증글 상세보기 화면 작업중

---
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/ViewProofByDateActivity.kt
@@ -0,0 +1,20 @@
+package com.eunhyung.daily10minutes_apiserverpractice_20210427
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProofByDateActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_proof_by_date)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+    override fun setValues() {
+
+    }
+}
