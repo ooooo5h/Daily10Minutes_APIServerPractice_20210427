62afa4193a1608c0daa37ed39c11b2677e4bba1d
전은형 <ooooo5h91@gmail.com>
2021-05-03 22:53:40 +0900

    인증글 파싱시 사진URL들도 목록으로 담아주도록 기능 추가

--- app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/datas/Proof.kt
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/datas/Proof.kt
@@ -7,6 +7,8 @@ class Proof {
     var id = 0
     var content = ""
 
+    val imageUrls = ArrayList<String>()
+
     companion object {
 
         fun getProofFromJson ( jsonObj : JSONObject) : Proof {
@@ -15,6 +17,14 @@ class Proof {
 
             proof.id = jsonObj.getInt("id")
             proof.content = jsonObj.getString("content")
+
+            val imagesArr = jsonObj.getJSONArray("images")
+
+            for (i in 0 until imagesArr.length()) {
+
+                proof.imageUrls.add( imagesArr.getJSONObject(i).getString("img_url"))
+
+            }
 
             return proof
 
