ddc2709909fdea2d34544c4f40b43383588e799b
전은형 <ooooo5h91@gmail.com>
2021-05-05 15:09:02 +0900

    액션바에 뒤로가기 버튼 추가 (메인화면에도 있네?)

--- app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/BaseActivity.kt
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/BaseActivity.kt
@@ -2,9 +2,11 @@ package com.eunhyung.daily10minutes_apiserverpractice_20210427
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.my_custom_action_bar.*
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -12,6 +14,8 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    lateinit var backImg : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,12 @@ abstract class BaseActivity : AppCompatActivity() {
 
         val myToolBar = defaultActionBar.customView.parent as Toolbar
         myToolBar.setContentInsetsAbsolute(0,0)
+
+
+        backImg = defaultActionBar.customView.findViewById(R.id.backImg)
+        backImg.setOnClickListener {
+            finish()
+        }
 
     }
 }
