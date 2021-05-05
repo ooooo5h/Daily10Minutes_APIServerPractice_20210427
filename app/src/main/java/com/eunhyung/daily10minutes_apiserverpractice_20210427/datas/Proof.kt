b4dca3f8b852ac49d52840241607ee57e644395c
전은형 <ooooo5h91@gmail.com>
2021-05-05 14:16:48 +0900

    인증글 파싱시 서버가 내려주는 일시 String -> 인증글의 시간(Calendar)로 저장

--- app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/datas/Proof.kt
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/datas/Proof.kt
@@ -1,6 +1,9 @@
 package com.eunhyung.daily10minutes_apiserverpractice_20210427.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Proof {
 
@@ -10,6 +13,9 @@ class Proof {
     val imageUrls = ArrayList<String>()
 
     lateinit var writer : User
+
+//    인증글 작성 일시 저장 변수 => 일시 저장은 캘린더. 캘린더는 내용물만 덮어쓰는 방식
+    val proofDateTime = Calendar.getInstance()
 
     companion object {
 
@@ -29,6 +35,16 @@ class Proof {
             }
 
             proof.writer = User.getUserFromJson( jsonObj.getJSONObject("user"))
+
+//            이 글이 작성된 일시도 파싱해야함.(서버가 주는 데이터는 String 이라서 Calendar 로 저장해야함!)
+//            1. String 을 파싱해서
+            val proofTimeStr = jsonObj.getString("proof_time")
+
+//            서버가 내려주는 양식 그대로 분석
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+//            2. 만들어진 일/시 를 Calendar 양식의 일/시로 변환
+            proof.proofDateTime.time = sdf.parse(proofTimeStr)
 
             return proof
 
