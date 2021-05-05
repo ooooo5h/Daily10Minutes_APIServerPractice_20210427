bec3403dd18737cfd2691fcedb7cabb68f0fc040
전은형 <ooooo5h91@gmail.com>
2021-05-05 14:54:10 +0900

    커스텀액션바 만들고 메인화면 반영

--- app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/BaseActivity.kt
+++ app/src/main/java/com/eunhyung/daily10minutes_apiserverpractice_20210427/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.eunhyung.daily10minutes_apiserverpractice_20210427
 
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -11,6 +13,14 @@ abstract class BaseActivity : AppCompatActivity() {
 
     fun setCustomActionBar() {
 
+        val defaultActionBar = supportActionBar!!
+
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        val myToolBar = defaultActionBar.customView.parent as Toolbar
+        myToolBar.setContentInsetsAbsolute(0,0)
 
     }
 }
